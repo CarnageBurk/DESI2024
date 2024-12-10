df15531b5a5ed11bff1c99bbb7c81ebda788075d
KarenClausen334 <karenclausen334@gmail.com>
2024-12-10 10:26:07 -0300

    cargo archivos

--- src/main/java/tuti/desi/ServletInitializer.java
+++ src/main/java/tuti/desi/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Desi2024Application.class);
+		return application.sources(BurkettMarquezClausenApplication.class);
 	}
 
 }
